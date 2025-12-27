1449aad1423ec7083e94192623f61358c180d018
Edson Petry <edsonlbpetry@gmail.com>
2025-12-26 23:32:56 -0500

    feat(sqlparser): introducing sql parser, hooray

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,13 @@
-use rquery::data_frame::ExecutionContext;
-use rquery::logical_plan::{BinaryExpr, Column, Literal};
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // SELECT first_name, last_name AS full_name
-    // FROM username
-    // WHERE identifier = '9012'
+    let sql = "SELECT a FROM table_1";
 
-    // let csv = CsvDataSource::try_new("test_data/csv/username.csv".to_string(), b';', true)?;
-    // let scan = Scan::new(&csv.filename, csv.clone(), None);
-    // let filter = Selection::new(
-    //     Arc::new(scan),
-    //     BinaryExpr::eq(Column::new("identifier"), Literal::string("9012")),
-    // );
-    // let project = Projection::new(
-    //     Arc::new(filter),
-    //     vec![
-    //         Box::new(Column::new("first_name")),
-    //         Box::new(Alias::new(Column::new("last_name"), "full_name")),
-    //     ],
-    // );
+    // parse to a Vec<Statement>
+    let ast = Parser::parse_sql(&GenericDialect, sql).unwrap();
 
-    let ctx = ExecutionContext::new(); // gives us a new ExeuctionContext object
-
-    let project = ctx
-        .csv("test_data/csv/username.csv", b';', true)
-        .filter(BinaryExpr::eq(
-            Column::new("Identifier"),
-            Literal::string("9012"),
-        ))
-        .project(vec![
-            Box::new(Column::new("first_name")),
-            Box::new(Column::new("last_name")),
-        ]);
-
-    println!("{}", project);
-    // Projection: #first_name, #last_name AS full_name
-    //     Filter: #identifier = '9012'
-    //         Scan: test_data/csv/username.csv; projection=None
+    println!("AST: {:?}", ast);
 
     Ok(())
 }
